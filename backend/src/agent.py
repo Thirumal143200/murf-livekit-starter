51b54857b7b487814bc9c2fd598fb69c06db7055
Thirumal143200 <thirumal4599@gmail.com>
2026-08-06 23:32:36 +0530

    Change TTS voice from 'Anisha' to 'en-IN-pooja'

--- backend/src/agent.py
+++ backend/src/agent.py
@@ -78,7 +78,7 @@ async def my_agent(ctx: JobContext):
         # Text-to-speech (TTS) is your agent's voice, turning the LLM's text into speech that the user can hear
         # See all available models as well as voice selections at https://docs.livekit.io/agents/models/tts/
         tts=murf.TTS(
-                voice="Anisha", 
+                voice="en-IN-pooja", 
                 locale="en-IN",
                 style="Conversation",
                 tokenizer=tokenize.basic.SentenceTokenizer(min_sentence_len=2),
@@ -92,6 +92,7 @@ async def my_agent(ctx: JobContext):
         # See more at https://docs.livekit.io/agents/build/audio/#preemptive-generation
         preemptive_generation=True,
     )
+
 
     # To use a realtime model instead of a voice pipeline, use the following session setup instead.
     # (Note: This is for the OpenAI Realtime API. For other providers, see https://docs.livekit.io/agents/models/realtime/))
